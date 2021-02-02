0ad3c9db0261e98beecd8c1a28c4ae0cea4e2095
Daitsuku <donlawat0@gmai.com>
2021-02-02 14:05:06 +0700

    1

---
+++ reverse_num.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/* ดลวัต เสมอวงค์ติ๊บ , แสดงเลข Reverse number */ 
+
+void main(){
+    system("cls");
+
+    int n,i;
+    printf("Please insert num : ");
+    scanf("%d",&n);
+
+//core
+
+    i = 0;
+    while (i != n){
+        printf("%d ",n);
+        n--;
+    }   
+}
